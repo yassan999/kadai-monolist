6b5fb003ddd6828f398c2d0983f616ba5b222a98
Takuya Yasuda <hoezurakakuna@gmail.com>
2017-05-20 09:41:23 +0000

    have ranking

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,5 @@ Rails.application.routes.draw do
     resources :items, only: [:show, :new]
     resources :ownerships, only: [:create, :destroy]
     get 'rankings/want', to: 'rankings#want'
+    get 'rankings/have', to: 'rankings#have'
 end
